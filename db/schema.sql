85dceade96ca0e5d417f272c3be003d4c1dc154c
Nik Cubrilovic <nikcub@gmail.com>
2014-06-03 16:53:37 +1000

    added page table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `firstname` varchar(256) DEFAULT NULL,
   `surname` varchar(256) DEFAULT NULL,
@@ -14,3 +14,17 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE IF NOT EXISTS `page` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `stub` varchar(256) DEFAULT NULL,
+  `content` TEXT DEFAULT NULL,
+  `parent` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+  `status` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+  `template` SMALLINT UNSIGNED,
+  `field` varchar(256) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `pubdate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
