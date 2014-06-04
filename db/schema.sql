c2e270c6d23ac507b3d8a2a4be937d1d77fc9dc3
Nik Cubrilovic <nikcub@gmail.com>
2014-06-04 20:41:54 +1000

    change default created and updated fields for tables

--- db/schema.sql
+++ db/schema.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(256) DEFAULT NULL,
   `password` varchar(512) DEFAULT NULL,
   `active` tinyint(1) DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `permissions` tinyint(4) DEFAULT '0',
   `lastlogin` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` timestamp DEFAULT 0,
+  `updated` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -24,8 +24,8 @@ CREATE TABLE IF NOT EXISTS `page` (
   `parent` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   `status` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   `template` SMALLINT UNSIGNED,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `pubdate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `pubdate` timestamp,
+  `created` timestamp DEFAULT 0,
+  `updated` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
